2ee2d512dbc1f14189995a8776c0396e73aa92a0
ElenaNorambuena <elena.norambuena.borquez@gmail.com>
2023-04-21 19:41:42 -0400

    actualización

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,22 @@
 const cipher = {
-  encode : function (text, offset){
+  encode : function (offset, text){
+    if (typeof offset !== 'number' || typeof text !== 'string'){
+      throw new TypeError('type error')
+    }
     if (!text)
       return "";
     const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     offset = (offset %26 + 26) % 26;
-    return text.replace(/[A-Z]/g, c=> letras[(letras.indexOf(c)+offset)%26]);
+    return text.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+offset)%26]);
   },
-  decode : function (text, offset){
+  decode : function (offset, text){
+    if (typeof offset !== 'number' || typeof text !== 'string'){
+      throw new TypeError('type error')
+    }
     if (!text)
       return "";
     const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    offset = (offset %26 + 26) % 26;
+    offset = (offset %26 - 26) % 26;
     return text.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)-offset)%26]);
   }
 }
