8e831da5be5fa12c7489405c8410e4700e32dcaf
ElenaNorambuena <elena.norambuena.borquez@gmail.com>
2023-04-21 03:22:16 -0400

    rearmado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,18 @@
 const cipher = {
-  // ...
-};
+  encode : function (text, offset){
+    if (!text)
+      return "";
+    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    offset = (offset %26 + 26) % 26;
+    return text.replace(/[A-Z]/g, c=> letras[(letras.indexOf(c)+offset)%26]);
+  },
+  decode : function (text, offset){
+    if (!text)
+      return "";
+    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    offset = (offset %26 + 26) % 26;
+    return text.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)-offset)%26]);
+  }
+}
 
 export default cipher;
