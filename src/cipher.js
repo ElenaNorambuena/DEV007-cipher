b0991f7aa89e170f186e52f359769b843c6d52e3
ElenaNorambuena <elena.norambuena.borquez@gmail.com>
2023-04-24 12:45:04 -0400

    casi listo

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,7 @@
 const cipher = {
   encode : function (offset, text){
-    if (typeof offset !== 'number' || typeof text !== 'string'){
-      throw new TypeError('type error')
+    if (typeof offset !== "number" || typeof text !== 'string'){
+      throw new TypeError("type error")
     }
     if (!text)
       return "";
@@ -10,8 +10,8 @@ const cipher = {
     return text.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+offset)%26]);
   },
   decode : function (offset, text){
-    if (typeof offset !== 'number' || typeof text !== 'string'){
-      throw new TypeError('type error')
+    if (typeof offset !== "number" || typeof text !== 'string'){
+      throw new TypeError("type error")
     }
     if (!text)
       return "";
@@ -19,6 +19,6 @@ const cipher = {
     offset = (offset %26 - 26) % 26;
     return text.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)-offset)%26]);
   }
-}
+};
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
 document.getElementById("encode").addEventListener("click", function(){
   const texto = document.getElementById("message").value;
   const offset = document.getElementById("offset").value;
-  document.getElementById("message2").value = cipher.encode(texto,offset);
+  document.getElementById("message2").value = cipher.encode(Number(offset), texto);
 }, true);
-document.getElementById("decode").addEventListener("click", function(){
 
+document.getElementById("decode").addEventListener("click", function(){
   const texto = document.getElementById("message").value;
   const offset = document.getElementById("offset").value;
-  document.getElementById("message2").value = cipher.decode(texto,offset);
+  document.getElementById("message2").value = cipher.decode(Number(offset), texto);
 }, true);
